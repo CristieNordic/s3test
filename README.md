71578e6d8ffdf1fb4b83737b44cbc9c1febc2a49
Christian Petersson <christian.petersson@cristie.se>
2020-10-27 15:42:42 +0100

    Added support for self signed certificate

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Python Tools for Cloudian and IBM Cloud Object Storage S3 interface
 ## Pre-reqs
 Make sure you running a virtualenv, Python3 and git on your machine.
 
-### Installation
+## Installation
 Clone this s3test project to your local machine.
 `git clone https://github.com/CristieNordic/s3test.git`
 
@@ -31,6 +31,9 @@ $ python s3.py --access-key <my-access-key> --secret_key <my-secret-key> --url <
  $ python s3.py --access-key <my-access-key> --secret_key <my-secret-key> --url <s3 url> --bucket <bucket name> --download --filename my_file.txt
  ```
 
+### Self Signed Certificate
+If you want to run with a self signed certificate, you can always add the switch `--insecure` to ignore any security warnings. 
+
 ### Using Config file
 
 If you don't want to specify your credentials, url or bucket everytime you can add that information to the `config.py` file.
@@ -38,31 +41,33 @@ If you don't want to specify your credentials, url or bucket everytime you can a
 ## Help Menu
 
 ```
-python s3.py --help
 usage: s3.py [-h] [--access-key ACCESS_KEY] [--secret-key SECRET_KEY]
-            [--bucket BUCKET] [--upload | --download] [--dir DIR]
-            [--filename FILENAME]
+             [--bucket BUCKET] [--upload | --download | --delete]
+             [--commMethod COMMMETHOD] [--url URL] [--remote REMOTE]
+             [--local LOCAL] [--filename FILENAME] [--insecure]
 
-Upload and download files from the FYNB server
+Upload and download files from a COS server
 
 optional arguments:
- -h, --help            show this help message and exit
- --access-key ACCESS_KEY
-                       Your access key
- --secret-key SECRET_KEY
-                       Your secret key
- --bucket BUCKET       The bucket name.
- --upload
- --download
- --delete
- --commMethod COMMMETHOD
-                       Change this value if not https, by typing
-                       --commMethod=http
- --url URL              The URL to the COS e.g. s3.company.com
- --remote REMOTE, -D REMOTE
-                       Remote directory at the S3 Storage
- --local LOCAL, -d LOCAL
-                       Local directory on your host
- --filename FILENAME   The filename to upload or download
+  -h, --help            show this help message and exit
+  --access-key ACCESS_KEY
+                        Your access key
+  --secret-key SECRET_KEY
+                        Your secret key
+  --bucket BUCKET       The bucket name.
+  --upload              Upload a file to your S3 Bucket
+  --download            Download a file to your S3 Bucket
+  --delete              Delete a file to your S3 Bucket
+  --commMethod COMMMETHOD
+                        Change this value if not https, by typing
+                        --commMethod=http
+  --url URL             The URL to the COS e.g. s3.company.com
+  --remote REMOTE, -D REMOTE
+                        Remote directory at the S3 Storage
+  --local LOCAL, -d LOCAL
+                        Local directory on your host
+  --filename FILENAME   The filename to upload or download
+  --insecure            Running over https with a self signed certifcate
+
 Contact support@cristie.se
 ```
--- s3.py
+++ s3.py
@@ -41,20 +41,31 @@ def set_configuration():
             problem('bucket')
             raise SystemExit()
 
+    # Approving non secure certifcate
+    if a.insecure:
+        ssl_verify = False
+    else:
+        try:
+            from config import bucket
+        except:
+            ssl_verify = True
+
+    # Set the value for your remote path at the S3 Bucket
     if a.remote:
         remote_directory = a.remote
     else:
         remote_directory = None
 
+    # Set the value for your local directory
     if a.local:
         local_directory = a.local
     else:
         local_directory = None
 
-    return (access_key, secret_key, url, bucket, remote_directory, local_directory)
+    return (access_key, secret_key, url, bucket, remote_directory, local_directory, ssl_verify)
 
-def download_file(access_key, secret_key, filename, url, bucket, remote_directory, local_directory):
-    s3_client = s3_client_connect(access_key, secret_key, url)
+def download_file(access_key, secret_key, filename, url, bucket, remote_directory, local_directory, ssl_verify):
+    s3_client = s3_client_connect(access_key, secret_key, url, ssl_verify)
     if local_directory is not None:
         local_filename = local_directory + '/' + filename
     else:
@@ -72,8 +83,8 @@ def download_file(access_key, secret_key, filename, url, bucket, remote_director
     except:
         print('Download failed to download {filename}'.format(**locals()))
 
-def delete_file(access_key, secret_key, filename, url, bucket, remote_directory):
-    s3 = s3_connect(access_key, secret_key, url)
+def delete_file(access_key, secret_key, filename, url, bucket, remote_directory, ssl_verify):
+    s3 = s3_connect(access_key, secret_key, url, ssl_verify)
 
     if remote_directory is not None:
         remote_filename = remote_directory + '/' + filename
@@ -85,9 +96,9 @@ def delete_file(access_key, secret_key, filename, url, bucket, remote_directory)
     except:
         print('Failed to delete file ' + remote_filename)
 
-def upload_file(access_key, secret_key, url, filename, bucket, remote_directory, local_directory):
+def upload_file(access_key, secret_key, url, filename, bucket, remote_directory, local_directory, ssl_verify):
     print(url)
-    s3 = s3_connect(access_key, secret_key, url)
+    s3 = s3_connect(access_key, secret_key, url, ssl_verify)
     if local_directory is not None:
         local_filename = local_directory + '/' + filename
     else:
@@ -105,32 +116,32 @@ def upload_file(access_key, secret_key, url, filename, bucket, remote_directory,
     #except:
     #    print('Unable to upload file {filename}'.format(**locals()))
 
-def list_file(access_key, secret_key, url, bucket,directory):
-    s3 = s3_connect(access_key, secret_key, url)
+def list_file(access_key, secret_key, url, bucket,directory, ssl_verify):
+    s3 = s3_connect(access_key, secret_key, url, ssl_verify)
     s3_bucket = s3.Bucket(name = bucket)
     for file in s3_bucket.objects.all():
         print(file)
 
-def s3_client_connect(access_key, secret_key, url):
-    return(boto3.client('s3', aws_access_key_id = access_key, aws_secret_access_key = secret_key, endpoint_url = url, verify=False))
+def s3_client_connect(access_key, secret_key, url, ssl_verify):
+    return(boto3.client('s3', aws_access_key_id = access_key, aws_secret_access_key = secret_key, endpoint_url = url, verify = ssl_verify))
 
-def s3_connect(access_key, secret_key, url):
-    return (boto3.resource('s3', aws_access_key_id = access_key, aws_secret_access_key = secret_key, endpoint_url = url, verify=False))
+def s3_connect(access_key, secret_key, url, ssl_verify):
+    return (boto3.resource('s3', aws_access_key_id = access_key, aws_secret_access_key = secret_key, endpoint_url = url, verify = ssl_verify))
 
 def problem(error):
     print("You haven't specify {error}".format(**locals()))
     print("Please you the --help for more information")
 
 def main():
-    access_key, secret_key, url, bucket, remote_directory, local_directory = set_configuration()
+    access_key, secret_key, url, bucket, remote_directory, local_directory, ssl_verify = set_configuration()
     if a.filename and a.download:
-        download_file(access_key, secret_key, a.filename, url, bucket, remote_directory, local_directory)
+        download_file(access_key, secret_key, a.filename, url, bucket, remote_directory, local_directory, ssl_verify)
     elif a.filename and a.delete:
-        delete_file(access_key, secret_key, a.filename, url, bucket, remote_directory)
+        delete_file(access_key, secret_key, a.filename, url, bucket, remote_directory, ssl_verify)
     elif a.filename and a.upload:
-        upload_file(access_key, secret_key, url,a.filename, bucket, remote_directory, local_directory)
+        upload_file(access_key, secret_key, url,a.filename, bucket, remote_directory, local_directory, ssl_verify)
     else:
-        list_file(access_key, secret_key, url, bucket, remote_directory)
+        list_file(access_key, secret_key, url, bucket, remote_directory, ssl_verify)
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser(
@@ -141,14 +152,15 @@ if __name__ == "__main__":
     p.add_argument("--access-key", help = "Your access key")
     p.add_argument("--secret-key", help = "Your secret key")
     p.add_argument("--bucket", help="The bucket name.")
-    g.add_argument("--upload", action="store_true")
-    g.add_argument("--download", action = "store_true")
-    g.add_argument("--delete", action = "store_true")
+    g.add_argument("--upload", action="store_true", help = "Upload a file to your S3 Bucket")
+    g.add_argument("--download", action = "store_true", help = "Download a file to your S3 Bucket")
+    g.add_argument("--delete", action = "store_true", help = "Delete a file to your S3 Bucket")
     p.add_argument("--commMethod", default="https", help="Change this value if not https, by typing --commMethod=http")
     p.add_argument("--url", help = "The URL to the COS e.g. s3.company.com")
     #g.add_argument("--list", action = "store_true")
     p.add_argument("--remote", "-D", help = "Remote directory at the S3 Storage")
     p.add_argument("--local", "-d", help = "Local directory on your host")
     p.add_argument("--filename", help = "The filename to upload or download")
+    p.add_argument("--insecure", action="store_true", help = "Running over https with a self signed certifcate")
     a = p.parse_args()
     main()
